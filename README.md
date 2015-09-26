93e46e769942e32cb40e99f43f6c2f8eba197af4
Alexey Bystrov <strikeentco@gmail.com>
2015-09-26 22:33:44 +0300

    1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# mime-kind
+mime-kind
+==========
+
+Detect the mime type of a Buffer, ReadStream, file path and file name.
+
+## Install
+```sh
+npm install mime-kind
+```
+
+## Usage
+
+```js
+var fs = require('fs');
+var mime = require('mime-kind');
+
+mime('.somefakeext'); // false
+mime(fs.createReadStream('./anonim.jpg')); // {ext: 'jpeg', mime: 'image/jpeg'}
+```
+
+## API
+
+### mime(data, [defaultValue])
+
+Returns an object (or `false` when no match) with:
+
+* `ext` - file type
+* `mime` - the [MIME type](http://en.wikipedia.org/wiki/Internet_media_type)
+
+### Params:
+
+* **data** (*Buffer|ReadStream|String*) - `Buffer`, `ReadStream`, file path or file name.
+* **[defaultValue]** (*String|Object*) - `String` or `Object` with value which will be returned if no match will be found. If `defaultValue` is incorrect returns `false`.
+
+```js
+var mime = require('mime-kind');
+
+mime('c:/anonim.jpeg'); // {ext: 'jpeg', mime: 'image/jpeg'}
+
+mime('.somefakeext', 'application/octet-stream'); // {ext: 'bin', mime: 'application/octet-stream'}
+mime('.somefakeext', {ext: 'mp4', mime: 'video/mp4'}); // {ext: 'mp4', mime: 'video/mp4'}
+mime('.somefakeext', 'ogg'); // {ext: 'ogg', mime: 'audio/ogg'}
+//but
+mime('.somefakeext', 'ogg3'); // false
+```
+
+```js
+var fs = require('fs');
+var mime = require('mime-kind');
+
+function chunkSync(data, length) {
+  var buf = new Buffer(length);
+  var fd = fs.openSync(data.path, data.flags);
+
+  fs.readSync(fd, buf, 0, length);
+  fs.closeSync(fd);
+
+  return buf;
+}
+
+mime(chunkSync({path: './anonim.jpeg', flags: 'r'}, 262); // {ext: 'jpeg', mime: 'image/jpeg'}
+```
+
+## License
+
+The MIT License (MIT)<br/>
+Copyright (c) 2015 Alexey Bystrov
---
+++ main.js
@@ -0,0 +1,111 @@
+'use strict';
+
+var fs = require('fs');
+var fileType = require('file-type');
+var mime = require('mime-types');
+
+function isObject(val) {
+  return Object.prototype.toString.call(val) === '[object Object]';
+}
+
+function isStream(stream) {
+  return stream && typeof stream === 'object' && typeof stream.pipe === 'function' && stream.readable !== false
+  && typeof stream._read === 'function' && typeof stream._readableState === 'object';
+}
+
+function isExists(path) {
+  try {
+    fs.accessSync(require('path').normalize(path));
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+function chunkSync(data, length) {
+  var buf = new Buffer(length);
+
+  if (!data.fd) {
+    data.path = require('path').normalize(data.path);
+    if (isExists(data.path)) {
+      data.fd = fs.openSync(data.path, data.flags, data.mode);
+    } else {
+      throw new Error('The file must be local and exists.');
+    }
+  }
+
+  fs.readSync(data.fd, buf, 0, length);
+  fs.closeSync(data.fd);
+
+  return buf;
+}
+
+function streamSync(stream, length) {
+  if (!stream.closed && !stream.destroyed && length > 0) {
+    var state = stream._readableState;
+    try {
+      var buf = chunkSync(stream, length/* || state.highWaterMark*/);
+      stream.emit('end');
+
+      return buf;
+    } catch (e) {
+      stream.emit('end');
+      throw new Error('The file must be local and exists.');
+    }
+  } else {
+    return false;
+  }
+};
+
+module.exports = function(data, defaultValue) {
+  var type;
+  var ext;
+
+  if (typeof data === 'string') {
+    type = mime.lookup(data);
+    if (!type && isExists(data)) {
+      type = fileType(chunkSync({path: data, flags: 'r'}, 262));
+    }
+  } else if (Buffer.isBuffer(data)) {
+    type = fileType(data);
+  } else if (isStream(data)) {
+    type = fileType(streamSync(data, 262));
+  }
+
+  if (type) {
+    !type.mime || (type = type.mime);
+    ext = mime.extension(type);
+    return {ext: ext, mime: type};
+  }
+
+  if (defaultValue) {
+    if (isObject(defaultValue)) {
+      if (defaultValue.ext) {
+        ext || (ext = defaultValue.ext);
+        type || (type = mime.lookup(ext));
+      }
+
+      if (defaultValue.mime || defaultValue.type) {
+        type || (type = defaultValue.mime || defaultValue.type);
+        ext || (ext = mime.extension(type));
+      }
+    } else if (typeof defaultValue === 'string') {
+      ext = mime.extension(defaultValue);
+      type = mime.lookup(defaultValue);
+
+      if (ext) {
+        type || (type = mime.lookup(ext));
+      }
+
+      if (type) {
+        ext || (ext = mime.extension(type));
+      }
+    }
+
+    if (type && ext) {
+      return {ext: ext, mime: type};
+    }
+  }
+
+  return false;
+};
